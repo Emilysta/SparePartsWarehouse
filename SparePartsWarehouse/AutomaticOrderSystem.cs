894c3b8aff22c2ff1dac0ec96bb9f81eaaff196e
Emilia Starczyk <emiliastarczyk@gmail.com>
2021-05-19 23:48:47 +0200

    Zamawianie części na podstawie danych historycznych - bierzemy pod uwagę 4 tygodnie wstecz i uśredniamy

---
+++ SparePartsWarehouse/AutomaticOrderSystem.cs
@@ -0,0 +1,84 @@
+﻿using SparePartsWarehouse.DatabaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparePartsWarehouse
+{
+
+    public class AutomaticOrderSystem
+    {
+
+        public static void OrderWeeklyAverageConsumption()
+        {
+            DetailOrderSystem.MakeDetailOrder(CheckAverageConsumption());
+        }
+
+        private static List<DetailOrderItem> CheckAverageConsumption()
+        {
+            ModelContext m_context = new ModelContext();
+
+            DateTime now = DateTime.UtcNow;
+            DateTime fourWeeksEarlier = now.AddDays(-28);
+
+            List<InvoiceItem> allInvoiceItems = new List<InvoiceItem>();
+            List<HistoricalInvoice> invoiceList = m_context.HistoricalInvoices.Where(x => DateTime.Compare(fourWeeksEarlier, x.InvoiceDate) <= 0).ToList();
+            foreach (HistoricalInvoice item in invoiceList)
+            {
+                List<InvoiceItem> tempList = m_context.InvoiceItems.Where(x => x.InvoiceId == item.InvoiceId).ToList();
+                foreach (InvoiceItem invoiceItem in tempList)
+                {
+                    if (allInvoiceItems.Any(x => x.ProductId == invoiceItem.ProductId))
+                    {
+                        var itemToChange = allInvoiceItems.Where(x => x.ProductId == invoiceItem.ProductId).FirstOrDefault();
+                        itemToChange.ProductQuantity += invoiceItem.ProductQuantity;
+                    }
+                    else
+                    {
+                        allInvoiceItems.Add(invoiceItem);
+                    }
+                }
+            }
+
+            List<DetailOrderItem> detailsToOrder = new List<DetailOrderItem>();
+
+            foreach (InvoiceItem item in allInvoiceItems)
+            {
+                List<ProdSpecification> prodSpecList = m_context.ProdSpecifications.Where(x => x.ProductId == item.ProductId).ToList();
+                foreach (ProdSpecification prodDetail in prodSpecList)
+                {
+                    if (detailsToOrder.Any(x => x.ItemId == prodDetail.DetailId))
+                    {
+                        var detailItem = detailsToOrder.Find(x => x.ItemId == prodDetail.DetailId);
+                        detailItem.Quantity += (int)prodDetail.DetailQuantity;
+                    }
+                    else
+                    {
+                        detailsToOrder.Add(new DetailOrderItem()
+                        {
+                            ItemName = m_context.Details.Where(x => x.DetailId == prodDetail.DetailId).FirstOrDefault().DetailName,
+                            ItemId = (int)prodDetail.DetailId,
+                            Quantity = (int)(prodDetail.DetailQuantity * item.ProductQuantity)
+                        });
+                    }
+                }
+            }
+
+            for (int i = 0; i < detailsToOrder.Count; i++)
+            {
+                DetailOrderItem detailOrderItem = detailsToOrder[i];
+                if (detailOrderItem.Quantity % 4 != 0)
+                {
+                    detailOrderItem.Quantity = detailOrderItem.Quantity / 4 + 1;
+                }
+                else
+                {
+                    detailOrderItem.Quantity = detailOrderItem.Quantity / 4;
+                }
+            }
+
+            return detailsToOrder;
+        }
+    }
+}
--- SparePartsWarehouse/Startup.cs
+++ SparePartsWarehouse/Startup.cs
@@ -5,6 +5,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Spi;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +27,9 @@ namespace SparePartsWarehouse
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var scheduler = StdSchedulerFactory.GetDefaultScheduler().GetAwaiter().GetResult();
+            services.AddSingleton(scheduler);
+            //services.AddHostedService<CustomQuartzHostedService>();
             services.AddRazorPages();
             services.AddSession( options =>
             {
@@ -33,6 +39,13 @@ namespace SparePartsWarehouse
             services.AddMemoryCache();
             services.AddDbContext<ModelContext>(options =>
             options.UseOracle("User Id=ADMIN;Password=lolp19;Data Source=192.168.1.30:51521/pdb1;"));
+
+            services.AddControllers();
+            services.AddSingleton<IJobFactory, CustomQuartzJobFactory>();
+            services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
+            services.AddSingleton<NotificationJob>();
+            services.AddSingleton(new JobMetadata(Guid.NewGuid(), typeof(NotificationJob), "Notification Job", "0 0 17 ? * FRI")); // 0/10 * * * * ?
+            services.AddHostedService<CustomQuartzHostedService>(); //0 0 22 ? * FRI 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
